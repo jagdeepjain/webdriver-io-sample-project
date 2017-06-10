ae68a3594c6c3569f38438d7a71c65216f90d518
jagdeepjain <jagdeep.jain@gmail.com>
2017-06-10 20:28:25 +0530

    first commit

---
+++ scripts/test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jjain on 5/5/2017.
+ */
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'firefox'
+    }
+};
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function (title) {
+    console.log('Title was: ' + title);
+})
+    .end();
---
+++ selenium/start-selenium-server.bat
@@ -0,0 +1,2 @@
+REM do not forget to add .exe after geckodriver
+java -jar -Dwebdriver.gecko.driver=geckodriver.exe selenium-server-standalone-3.4.0.jar
